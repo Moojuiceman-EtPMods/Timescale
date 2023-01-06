7f7249acb0504c008863af671ff6ea66fabf2685
Alex Barrett <moojuiceman@hotmail.co.uk>
2023-01-06 08:50:50 +0000

    Initial port/fixes

---
+++ Plugin.cs
@@ -0,0 +1,72 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Timescale
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        static ManualLogSource logger;
+        public static ConfigEntry<KeyboardShortcut> openMenuKey;
+
+        private void Awake()
+        {
+            openMenuKey = Config.Bind("General", "Open Menu Key", new KeyboardShortcut(KeyCode.F2), "Key to open timescale menu");
+
+            // Plugin startup logic
+            logger = Logger;
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded");
+            Logger.LogInfo($"Patching...");
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+            Logger.LogInfo($"Patched");
+        }
+
+        [HarmonyPatch(typeof(PlayerManager), "Start")]
+        [HarmonyPrefix]
+        static void Start_Prefix()
+        {
+            new GameObject("__Timescale__").AddComponent<TimescaleMain>().Init(openMenuKey);
+        }
+    }
+
+
+    internal class TimescaleMain : MonoBehaviour
+    {
+        private bool visible;
+        private float value = 1f;
+        static ConfigEntry<KeyboardShortcut> openMenuKey;
+
+        public void Init(ConfigEntry<KeyboardShortcut> menuKey)
+        {
+            openMenuKey = menuKey;
+        }
+
+        private void OnGUI()
+        {
+            if (this.visible)
+            {
+                GUI.Box(new Rect(50f, 50f, 450f, 40f), "");
+                GUI.Label(new Rect(75f, 60f, 100f, 40f), "Timescale:");
+                this.value = GUI.HorizontalSlider(new Rect(175f, 62.5f, 300f, 20f), this.value, 1f, 60f);
+                Time.timeScale = this.value;
+            }
+        }
+
+        private void Update()
+        {
+            if (openMenuKey.Value.IsDown())
+            {
+                this.visible = !this.visible;
+                if (this.visible)
+                {
+                    CursorManager.Instance.ShowCursor();
+                    return;
+                }
+                CursorManager.Instance.HideCursor();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+# Timescale
+
+This is a mod for the game Escape The Pacific by Gamers4Gamers Team. You can get it here: https://store.steampowered.com/app/655290/Escape_The_Pacific/
+
+This is a port of the ModAPI mod by Moritz as ModAPI no longer supports the game. The original mod is here https://modapi.survivetheforest.net/mod/71/chronos-god-of-time
+
+#### What it does
+
+Change the speed of time.
+
+#### Save file backup
+
+I try to not affect saves with my mods, but taking backups is recommended. This is also how you can share save files for deubgging.
+
+* Saves are located at C:\Users\<Username>\AppData\LocalLow\Gamers4Gamers Team\Escape The Pacific\SavesDir
+* Make a copy of the folder, either to another location or as a new folder name in the same place
+* If sharing the save, zip the folder for easier transfer
+
+#### Installation
+
+* Download BepInEx 5 64-bit. Here is a direct link to version 5.4.21 which works with the game https://github.com/BepInEx/BepInEx/releases/download/v5.4.21/BepInEx_x64_5.4.21.0.zip
+* Unzip it into the game folder, next to EscapeThePacific.exe. Find it via Steam by right clicking the game > Manage > Browse local files
+* Start the game once so BepInEx creates the necessary folders
+* Grab the latest .dll from the releases page and put it in the BepInEx\plugins folder
+ 
+#### Configuration
+
+Mod options are stored in BepInEx\config\<Modname>.cfg
+
+* Open Menu Key (Default F2) - Keybind to open timescale change menu
